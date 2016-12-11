8a5aeaee80cf11e198ca8a27f94248b9753eb3fa
Seth Rider <seth.rider@gmail.com>
2016-12-11 11:08:14 +0000

    Switch to grunt for compiling jsx

---
+++ WindowsServicePortal/Gruntfile.js
@@ -0,0 +1,35 @@
+﻿/// <binding ProjectOpened='watch' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: {
+            contents: ["wwwroot/js/*"]
+        },
+        babel: {
+            options: {
+                plugins: ["transform-react-jsx"],
+                presets: ["es2015", "react"]
+            },
+            jsx: {
+                files: [{
+                    expand: true,
+                    cwd: "jssrc/",
+                    src: ["*.jsx"],
+                    dest: "wwwroot/js/",
+                    ext: ".js"
+                }]
+            }
+        },
+        watch: {
+            files: ["jssrc/*"],
+            tasks: ["all"]
+        }
+    });
+    grunt.registerTask("all", ["clean", "babel"]);
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-babel");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+};
---
+++ WindowsServicePortal/jssrc/app.jsx
@@ -0,0 +1,100 @@
+﻿var statusUrlRoot = "/api/windowsservice/status/";
+var machineListUrl = "/api/windowsservice/machinenames";
+
+var MachineList = React.createClass({
+    getInitialState: function () {
+        return { data: [] };
+    },
+    componentWillMount: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    render: function () {
+        var machineNodes = this.state.data.map(function (machine) {
+            return (
+                <Machine name={machine.name}>
+                </Machine>
+                );
+        })
+        return (
+    <div className="machineList">
+        {machineNodes}
+    </div>)
+        ;
+    }
+});
+
+var Machine = React.createClass({
+    getInitialState: function () {
+        return { loadServiceList: true };
+    },
+    render: function () {
+        return (
+            <div className="machine">
+            <h2 className="machineName" onClick={this.onClick}>
+                {this.props.name}
+            </h2>
+                {
+                this.state.loadServiceList
+                ? <ServiceList machineName={this.props.name} pollInterval={3000} />
+	            : null
+                }
+            </div>
+	    );
+    },
+    onClick: function () {
+        this.setState({ loadServiceList: !this.state.loadServiceList });
+    }
+});
+
+var ServiceList = React.createClass({
+    loadServiceStatusFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', statusUrlRoot + this.props.machineName, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    getInitialState: function () {
+        return { data: [] };
+    },
+    componentWillMount: function () {
+        this.loadServiceStatusFromServer();
+        window.setInterval(this.loadServiceStatusFromServer, this.props.pollInterval)
+    },
+    render: function () {
+        var serviceNodes = this.state.data.map(function (service) {
+            return (
+                <Service name={service.name} status={service.status} />
+                );
+        })
+        return (
+    <table className="serviceList">
+        <tbody>{serviceNodes}</tbody>
+    </table>)
+        ;
+    }
+});
+
+var Service = React.createClass({
+    render: function () {
+        return (
+              <tr className="serviceRow">
+                <td className="serviceRowName">{this.props.name}</td>
+                <td className="serviceRowStatus">{this.props.status}</td>
+              </tr>
+      );
+    }
+});
+
+ReactDOM.render(
+  <MachineList url={machineListUrl} />,
+  document.getElementById('content')
+);
